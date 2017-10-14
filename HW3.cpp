2646e19574e634d59d25cad3dc3ab91b6fb8e566
Louislar <a8945267@gmail.com>
2017-10-14 20:18:07 +0800

    upload cpp

---
+++ HW3.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+
+using namespace std;
+
+class team
+{
+    public:
+        team()
+        {
+            elementNum=0;
+        }
+        int element[1000];
+        int elementNum;
+};
+
+main()
+{
+    team* queue=new team[1000];//what's the different between
+                               //1. team queue[1000]
+                               //2. team* queue=new team[1000]
+                               //heap? stack? what's the different
+                               //https://stackoverflow.com/questions/22578879/is-it-possible-to-store-10-million-numbers-in-array
+    //input
+    {
+        int teamNum;
+        cin>>teamNum;
+        if(teamNum==0)
+        {
+
+        }
+
+    }
+
+
+}
